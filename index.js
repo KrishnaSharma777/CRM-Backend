56a55f917284438693d53e70e314ed345b6bda44
AjayOnly1Loan <ajay@only1loan.com>
2024-09-19 12:43:21 +0530

    Inital commit

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import userRoutes from './routes/userroutes.js'; // Import routes
-import cors from 'cors'; // Import cors
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors"; // Import cors
+import connectDB from "./config/db.js";
+import "dotenv/config.js";
+import morgan from "morgan";
+import userRoutes from "./routes/userroutes.js"; // Import routes
+
+const PORT = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Middleware configuration
-app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from this origin
-    credentials: true // Allow credentials (cookies, authorization headers, etc.)
-}));
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Allow requests from this origin
+        credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    })
+);
 
 app.use(bodyParser.json()); // Middleware to parse JSON requests
 
-const url = "mongodb://localhost:27017/login"; // MongoDB connection URL
-
-mongoose.connect(url)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log("MongoDB connection error:", err);
-    });
-
 // Routes
-app.use('/api/users', userRoutes); // Use the user routes
+app.get("/", (req, res) => {
+    res.send("API is running.......");
+});
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+// app.use("/api/users", userRoutes); // Use the user routes
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
